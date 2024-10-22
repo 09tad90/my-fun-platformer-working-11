ab5182a9abd2d6c02ecf051330688d1bf0819817
09tad90 <153658118+09tad90@users.noreply.github.com>
2024-10-22 12:55:25 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . . f f e e e e e e f f . . . 
+. . . f e 2 2 2 2 2 2 e f . . . 
+. . . f 2 f f f f f f 2 f . . . 
+. . . f f f e e e e f f f . . . 
+. . . f f b f 4 4 f b f f . . . 
+. . . f 4 1 f d d f 1 4 f . . . 
+. . . f e 4 d d d d 4 e f . . . 
+. . . f f 2 2 2 2 e d d f . . . 
+. . . f f 2 2 2 2 e d d f . . . 
+. . . . f 4 4 5 5 f e f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
